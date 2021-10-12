0bad8e4f2f2d02cd3c3b36f8e3cff4d699230db7
Lucas Martins <lmartins333@hotmail.com>
2021-10-11 22:56:59 -0300

    Primeiro Commit

---
+++ src/test/java/ola/mundo/olamundo/OlamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package ola.mundo.olamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
